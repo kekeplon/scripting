0522a0e5feec2f667f46f3fddec041371c9d9ae1
kekeplon <150566296+kekeplon@users.noreply.github.com>
2023-12-09 23:34:42 +1000

    fire

---
+++ Missile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    //Скорость полёта огненного шара
+    public float speed;
+
+    void Update()
+    {
+        Destroy(gameObject, 3);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        Enemy enemy = other.GetComponent<Enemy>();
+        Destroy(enemy.gameObject);
+        Destroy(gameObject);
+    }
+
+}
+
+
+
+
